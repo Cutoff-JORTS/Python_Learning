d6bf44940eb779e47e6c784395a4211d1541b10e
Jason Ort <jorts.tech@gmail.com>
2024-04-24 20:01:04 -0400

    initial commit

---
+++ EvenOdd.py
@@ -0,0 +1,14 @@
+numbers = [] #fill with list of numbers
+
+even_numbers = []
+odd_numbers = []
+
+for num in numbers:
+	if num % 2 == 0:
+		even_numbers.append(num)
+	
+	else
+		odd_numbers.append(num)
+
+print("Even numbers: ", even_numbers)
+print("Odd numbers: ", odd_numbers)
---
+++ README.md
@@ -0,0 +1,4 @@
+This repo contains very basic python scripts written while self-teaching the basics of language structure & syntax for simple elements like lists, dictionaries, loops, etc.
+
+Thank you for reading.
+-jort
